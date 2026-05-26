32ed1be73450a50e0938fa0a79d74415c6240900
SeanglyDev <Seangly.Dev@gmail.com>
2026-05-26 15:45:50 +0700

    fix button view and translate

--- resources/views/frontend/classic/index.blade.php
+++ resources/views/frontend/classic/index.blade.php
@@ -478,8 +478,8 @@
                                     {{ $category->getTranslation('name') }}
                                 </h5>
 
-                                <small class="text-light">
-                                    {{ $category->meta_description }}
+                                <small class="text-light fs-14">
+                                   {{ $category->getTranslation('meta_description') }}
                                 </small>
                             </div>
 
@@ -765,6 +765,9 @@
     @if (count($featured_categories) > 0)
         @foreach($featured_categories as $category)
             @if(count($category->products) > 0)
+            @php
+                $featured_category_section_id = 'section_featured_category_' . $category->id;
+            @endphp
             <section class="mb-2 mb-md-3 mt-2 mt-md-3">
                 <div class="container">
                     <div class="bg-white">
@@ -775,14 +778,16 @@
                                 <span class="">{{ $category->getTranslation('name') }}</span>
                             </h3>
                             <!-- Links -->
-                            <div class="d-flex">
-                                <a class="text-blue fs-10 fs-md-12 fw-700 hov-text-primary animate-underline-primary"
+                            <div class="d-flex align-items-center">
+                                <a type="button" class="arrow-prev slide-arrow link-disable text-secondary mr-2" onclick="clickToSlide('slick-prev','{{ $featured_category_section_id }}')"><i class="las la-angle-left fs-20 fw-600"></i></a>
+                                <a class="text-blue fs-12 fw-700 hov-text-primary mx-2"
                                     href="{{ route('products.category', $category->slug) }}">{{ translate('View All') }}</a>
+                                <a type="button" class="arrow-next slide-arrow text-secondary ml-2" onclick="clickToSlide('slick-next','{{ $featured_category_section_id }}')"><i class="las la-angle-right fs-20 fw-600"></i></a>
                             </div>
                         </div>
                     </div>
                     <!-- Products Section -->
-                    <div class="px-sm-3">
+                    <div id="{{ $featured_category_section_id }}" class="px-sm-3">
                         <div class="aiz-carousel arrow-none sm-gutters-16" data-items="6" data-xl-items="5" data-lg-items="4"  data-md-items="3" data-sm-items="2" data-xs-items="2" data-arrows='true' data-infinite='false'>
                             @foreach ($category->products as $key => $new_product)
                             <div class="carousel-box px-3 position-relative has-transition border-right border-top border-bottom @if($key == 0) border-left @endif hov-animate-outline">
--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -406,7 +406,7 @@
 
                 </div>
 
-                <button type="button" class="close absolute-top-right btn-icon close z-1 btn-circle bg-gray mr-2 mt-2 d-flex justify-content-center align-items-center" data-dismiss="modal" aria-label="Close" style="background: #ededf2; width: calc(2rem + 2px); height: calc(2rem + 2px);">
+                <button type="button" class="close absolute-top-right btn-icon close z-1 btn-circle bg-gray mr-2 mt-2 d-flex justify-content-center align-items-center" data-dismiss="modal" onclick="$('#addToCart').modal('hide')" aria-label="Close" style="background: #ededf2; width: calc(2rem + 2px); height: calc(2rem + 2px);">
 
                     <span aria-hidden="true" class="fs-24 fw-700" style="margin-left: 2px;">&times;</span>
 
