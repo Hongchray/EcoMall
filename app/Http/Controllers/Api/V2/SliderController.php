66602e4898a7104784d9cf9e52e6149547ce896d
SeanglyDev <Seangly.Dev@gmail.com>
2026-09-14 11:18:01 +0700

    make api v2 for banner mobile

--- app/Http/Controllers/Api/V2/SliderController.php
+++ app/Http/Controllers/Api/V2/SliderController.php
@@ -3,13 +3,20 @@
 namespace App\Http\Controllers\Api\V2;
 
 use App\Http\Resources\V2\SliderCollection;
+use App\Models\Banner;
 use Cache;
 
 class SliderController extends Controller
 {
     public function sliders()
     {
-        return new SliderCollection(get_setting('home_slider_images') != null ? json_decode(get_setting('home_slider_images'), true) : []);
+        $images = Banner::where('position', 'slider')
+            ->where('status', 1)
+            ->orderBy('created_at', 'asc')
+            ->pluck('image')
+            ->toArray();
+
+        return new SliderCollection($images);
     }
 
     public function bannerOne()
